d10bf0be5971ad9c0dd11fcdd1c4c4f20a996c33
Omer Gronich <gronichomer@gmail.com>
2020-12-19 17:43:55 +0200

    show example of both reaction and autorun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,36 @@ configure({observableRequiresReaction: true});
              template: `
                <h2>Open the console to see the warning</h2>
                <div *mobxAutorun>
-                 {{state | json}}
+                 {{autorun_state | json}}
                </div>
+               <button (click)="removeAutorun()">remove</button>
+               <br>
+               <hr>
+               <div *mobxReaction="getState.bind(this)">
+                 {{reaction_state | json}}
+               </div>
+               <button (click)="removeReaction()">remove</button>
              `,
              styles  : []
            })
 export class AppComponent {
 
-  state = ['random state', 'random state', 'random state'];
+  autorun_state = ['autorun state', 'autorun state', 'autorun state'];
+  reaction_state = ['reaction state', 'reaction state', 'reaction state'];
 
   constructor() {
     makeAutoObservable(this);
+  }
+
+  getState() {
+    return this.reaction_state;
+  }
+
+  removeAutorun() {
+    this.autorun_state.splice(this.autorun_state.length - 1, 1);
+  }
+
+  removeReaction() {
+    this.reaction_state.splice(this.reaction_state.length - 1, 1);
   }
 }
