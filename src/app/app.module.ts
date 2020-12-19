b20ff654b2a0faf5f9445bc34efda1f47f4774a1
Omer Gronich <gronichomer@gmail.com>
2020-12-19 17:35:14 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule}      from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent}      from './app.component';
+import {MobxAngularModule} from 'mobx-angular';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+            declarations: [
+              AppComponent
+            ],
+            imports     : [
+              BrowserModule,
+              MobxAngularModule
+            ],
+            providers   : [],
+            bootstrap   : [AppComponent]
+          })
+export class AppModule {
+}
